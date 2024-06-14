56c2cba873c0f43024d6996f544662b055ca8c39
Gabriel Reis <gabrieldreisilva4@gmail.com>
2024-06-14 11:28:05 -0300

    :+1: adicionador de link finalizado

--- atv2/automanager/src/main/java/com/autobots/automanager/repositorios/DocumentoRepositorio.java
+++ atv2/automanager/src/main/java/com/autobots/automanager/repositorios/DocumentoRepositorio.java
@@ -1,3 +1,9 @@
-public class DocumentoRepositorio {
-    
+package com.autobots.automanager.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.autobots.automanager.entidades.Documento;
+
+public interface DocumentoRepositorio extends JpaRepository<Documento, Long>{
+
 }
--- atv2/automanager/src/main/java/com/autobots/automanager/repositorios/EnderecoRepositorio.java
+++ atv2/automanager/src/main/java/com/autobots/automanager/repositorios/EnderecoRepositorio.java
@@ -1,3 +1,9 @@
-public class EnderecoRepositorio {
-    
+package com.autobots.automanager.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.autobots.automanager.entidades.Endereco;
+
+public interface EnderecoRepositorio  extends JpaRepository<Endereco, Long>{
+
 }
