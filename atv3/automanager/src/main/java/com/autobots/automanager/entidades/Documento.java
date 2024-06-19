1607d3ca0f45e9c24ab3e6d28199b943673e1dbf
Gabriel Reis <gabrieldreisilva4@gmail.com>
2024-06-18 22:11:56 -0300

    pom xml

--- atv3/automanager/src/main/java/com/autobots/automanager/entidades/Documento.java
+++ atv3/automanager/src/main/java/com/autobots/automanager/entidades/Documento.java
@@ -1,11 +1,15 @@
 package com.autobots.automanager.entidades;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import org.springframework.hateoas.RepresentationModel;
+import com.autobots.automanager.enumeracoes.TipoDocumento;
+
 import lombok.Data;
 
 @Data
@@ -14,8 +18,13 @@ public class Documento extends RepresentationModel<Documento>{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column
-	private String tipo;
-	@Column(unique = true)
+	
+	@Column(nullable = false)
+	private TipoDocumento tipo;
+	
+	@Column(nullable = false)
+	private Date dataEmissao;
+	
+	@Column(unique = true, nullable = false)
 	private String numero;
 }
